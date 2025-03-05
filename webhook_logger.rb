a8f42cb40fd1d8c6ac9ba94e23fa31a40ca7f172
Naoyuki Ishida <39.ishida@gmail.com>
2025-03-04 22:59:05 -0800

    fix: Made Pumatra class

--- webhook_logger.rb
+++ webhook_logger.rb
@@ -2,29 +2,37 @@ require 'sinatra'
 require 'json'
 require 'logger'
 
-log_file = File.expand_path('webhook.log', __dir__)
-logger = Logger.new(log_file)
-
-get '/' do
-  'Hello, World!'
+configure do
+  set :server, :puma
 end
 
-post '/squire/webhook' do
-  begin
-    request_body = request.body.read
-    logger.info("Received webhook at #{Time.now}")
-    logger.info("Request body: #{request_body}")
+class Pumatra < Sinatra::Base
+  log_file = File.expand_path('webhook.log', __dir__)
+  logger = Logger.new(log_file)
 
-    payload = JSON.parse(request_body)
-
-    logger.info("Event type: #{payload['type']}")
-
-    status 200
-    "OK"
-
-  rescue => e
-    logger.error("Failed to process webhook: #{e.message}")
-    status 500
-    "Error"
+  get '/' do
+    'Hello, World!'
   end
+
+  post '/squire/webhook' do
+    begin
+      request_body = request.body.read
+      logger.info("Received webhook at #{Time.now}")
+      logger.info("Request body: #{request_body}")
+
+      payload = JSON.parse(request_body)
+
+      logger.info("Event type: #{payload['type']}")
+
+      status 200
+      "OK"
+
+    rescue => e
+      logger.error("Failed to process webhook: #{e.message}")
+      status 500
+      "Error"
+    end
+  end
+
+  run! if app_file == $0
 end
